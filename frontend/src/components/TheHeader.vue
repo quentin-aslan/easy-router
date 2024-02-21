8eaf81a58c590c1a1c2a51f43bcf1b6f1239dd97
Aslan Quentin <quentin.aslan@outlook.com>
2024-02-21 18:57:19 -0500

    :rocket: Release v1

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar bg-base-100">
     <div class="flex-1">
-      <a @click="router.push('/')" class="btn btn-ghost text-xl">🛜 Router Easy 🛜</a
+      <a @click="router.push('/')" class="btn btn-ghost text-xl">🛜 Router Easy 🛜 v{{ version }}</a
       >
     </div>
   </div>
@@ -9,4 +9,7 @@
 
 <script lang="ts" setup>
 import router from "../router";
+import pkg from '../../package.json';
+
+const version = pkg.version;
 </script>
