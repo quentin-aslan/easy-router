21e91ac2590a7e611f6ab01032fb5908562c0d1e
Aslan Quentin <quentin.aslan@outlook.com>
2024-02-26 16:29:49 -0500

    :sparkles: Hotspot config subnet added on HotspotSettings.vue + Read Only

--- frontend/src/components/HotspotSettings.vue
+++ frontend/src/components/HotspotSettings.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <CardTemplate title="Hotspot Settings">
+  <CardTemplate title="Hotspot Settings (Read only)">
     <div v-if="isGetHotspotConfigError" role="alert" class="alert alert-error">
       <ErrorIcon />
       <span>Error when retrieving the data</span>
@@ -14,7 +14,7 @@
       <form class="form-control w-full flex flex-col gap-5" @submit.prevent="submitForm">
         <div class="form-control w-full gap-1 text-left">
           <label for="hotspot-ssid" class="label">Hotspot Name (SSID)</label>
-          <input id="hotspot-ssid" v-model="ssid" type="text" class="input input-bordered" placeholder="captain" />
+          <input :disabled="READ_ONLY" id="hotspot-ssid" v-model="ssid" type="text" class="input input-bordered" placeholder="captain" />
         </div>
 
 
@@ -22,7 +22,7 @@
           <label for="hotspot-password" class="label">Password</label>
           <label class="input input-bordered flex items-center gap-2">
           <PasswordIcon />
-          <input ref="passwordInput" id="hotspot-password" v-model="password" :type="(showPassword) ? 'text' : 'password'" class="grow" placeholder="Wifi Password" />
+          <input :disabled="READ_ONLY" ref="passwordInput" id="hotspot-password" v-model="password" :type="(showPassword) ? 'text' : 'password'" class="grow" placeholder="Wifi Password" />
           <SwapCheckbox v-model="showPassword" />
           </label>
         </div>
@@ -42,7 +42,12 @@
           </div>
         </div>
 
-        <button type="submit" class="btn btn-neutral" :disabled="isSubmitDisabled" >
+        <div class="form-control w-full gap-1 text-left">
+          <label for="hotspot-subnet" class="label">Subnet (Address/ Netmask)</label>
+          <input :disabled="READ_ONLY" id="hotspot-subnet" v-model="subnet" type="text" class="input input-bordered" placeholder="10.42.0.1/24" />
+        </div>
+
+        <button  type="submit" class="btn btn-neutral" :disabled="isSubmitDisabled" >
         Apply Changes
         </button>
       </form>
@@ -62,6 +67,8 @@ import {useManageHotspot} from "../composable/use-manage-hotspot";
 import SwapCheckbox from "./SwapCheckbox.vue";
 import ErrorIcon from "./icons/ErrorIcon.vue";
 
+const READ_ONLY = true
+
 const { hotspotConfig, isGetHotspotConfigLoading, isGetHotspotConfigError } = useManageHotspot()
 
 const ssid = ref('');
@@ -69,14 +76,17 @@ const password = ref(hotspotConfig.value?.password || '');
 const showPassword = ref(false);
 const band = ref(hotspotConfig.value?.band || BandEnum["2.4G"]);
 const bandDisabled = computed(() => true) // Backend not ready yet
+const subnet = ref('')
 
 watch(hotspotConfig, (newValue) => {
   ssid.value = newValue.ssid
   password.value = newValue.password
   band.value = newValue.band
+  subnet.value = newValue.subnet
 }, {immediate: true})
 
 const isSubmitDisabled = computed(() => (
+    READ_ONLY ||
     ssid.value === '' || password.value.length < 3 ||  isGetHotspotConfigLoading.value ||
     ssid.value === hotspotConfig.value?.ssid && password.value === hotspotConfig.value?.password && band.value === hotspotConfig.value?.band
     )
